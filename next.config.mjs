a1afe9d511b8c80f1e36a3327e2920488994ee97
zubaerSumon <zubaer@corexlab.dev>
2024-09-09 17:34:13 +0600

    images domain added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "180b-59-153-103-128.ngrok-free.app"],
+    domains: ["localhost","55ed-59-153-103-142.ngrok-free.app"],
   },
 };
 
