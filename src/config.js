127669abd4b3bc2ef92a22a9a90da306447732d1
zubaerSumon <zubaer@corexlab.dev>
2024-09-11 11:42:02 +0600

    upadated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  api: "http://localhost:1338",
-  //api: "https://b9cd-59-153-103-141.ngrok-free.app",
+  //api: "http://localhost:1338",
+  api: "https://b9cd-59-153-103-141.ngrok-free.app",
 };
