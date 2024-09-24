0432536ec9a9667c46038e0ed29ab53bb9e0ed16
zubaer_sumon <zubaer.rahman.cse@gmail.com>
2024-09-24 13:47:38 +0600

    ngrok url replaced

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
   //api: "http://localhost:1338",
-  api: "https://c3cd-37-111-246-115.ngrok-free.app",
+  api: "https://90c8-2400-c600-3660-6b26-f8f6-17a-93bc-3837.ngrok-free.app",
 };
